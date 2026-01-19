a9c2bc11d979802300f7595badcb2d20b1c335e0
greciacoria <greciacoria77@gmail.com>
2026-01-19 02:01:51 -0300

    Add Pydantic models for task management

---
+++ schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class TaskCreate(BaseModel):
+    title: str
+
+class TaskUpdate(BaseModel):
+    title: str
+    done: bool
+
+class TaskOut(BaseModel):
+    id: int
+    title: str
+    done: bool
+
+    class Config:
+        from_attributes = True
